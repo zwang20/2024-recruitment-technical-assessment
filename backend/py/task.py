a95dd8f1776b2d682f26be084b79bc275b1e027a
Michael Wang <michael_wzq@icloud.com>
2024-02-09 16:25:33 +1100

    backend

--- backend/py/task.py
+++ backend/py/task.py
@@ -13,21 +13,73 @@ class File:
 Task 1
 """
 def leafFiles(files: list[File]) -> list[str]:
-    return []
+
+    # get the set of all parent (non-leaf) files
+    parent_files: set[int] = set()
+    for file in files:
+        parent_files.add(file.parent)
+    
+    # the leaf files will be all the files that are not parents
+    leaf_files: list[str] = []
+    for file in files:
+        if file.id not in parent_files:
+            leaf_files.append(file.name)
+
+    return leaf_files
 
 
 """
 Task 2
 """
 def kLargestCategories(files: list[File], k: int) -> list[str]:
-    return []
 
+    # create a dictionary with category as key and 
+    # the set of ids as values
+    categories: dict[str, set[int]] = {}
+    for file in files:
+        for category in file.categories:
+            if category not in categories.keys():  
+                categories[category] = set()
+            categories[category].add(file.id)
+    
+    # sort them in alphabetical order before sorting them by size
+    alphabetical_categories = sorted(list(categories.items()), key=lambda x: x[0])
+    largest_categories = sorted(alphabetical_categories, key=lambda category: -len(category[1]))
+    k_largest_categories = [category[0] for category in largest_categories[:k]]
+    return k_largest_categories
 
 """
 Task 3
 """
+import typing
 def largestFileSize(files: list[File]) -> int:
-    return 0
+    # this is a misleading name for a function
+    # prehaps consider largestFileSystemEntry?
+
+    @dataclass
+    class File_Node:
+        id: int
+        size: int
+        parent: int
+        children: list[typing.Self]
+
+    # create a dict of nodes
+    nodes: dict[int, File_Node] = {}
+    for file in files:
+        nodes[file.id] = File_Node(file.id, file.size, file.parent, list())
+    
+    # add nodes to their parents
+    for node in nodes.values():
+        if node.parent != -1:
+            nodes[node.parent].children.append(node)
+
+    # return the max size via recursively calculating the size
+    def size(node: File_Node) -> int:
+        return node.size + sum(map(size, node.children))
+    return max({
+        size(node) for node in nodes.values()
+        if node.parent == -1
+    })
 
 
 if __name__ == '__main__':
